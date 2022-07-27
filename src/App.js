e24e60e477e9f5d65743e0134f7fef8d599becb7
Abdallah Smadi <abd.smadi41@gmail.com>
2022-07-27 15:56:07 +0300

    edit lab 03

--- src/App.js
+++ src/App.js
@@ -2,18 +2,43 @@ import './App.css';
 import Header from './header';
 import Main from './main';
 import Footer from './footer';
+import React from 'react';
+import data from "./data.json"
+import Selected from './selectedBeast';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      allBeast : data ,
+      selectedBeast :{},
+      showModal: false
+    }
+  }
+
+  displayModal =(beast)=>{
+    this.setState({
+      showModal :true,
+      selectedBeast :beast
+    })
+  }
+
+  handleClose = () =>{
+    this.setState({
+      showModal :false
+    })
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
         <Header/>
-        <Main/>
+        <Main allBeast={this.state.allBeast} displayModal={this.displayModal}/>
+        <Selected show={this.state.showModal} handleClose={this.handleClose} selectedBeast={this.state.selectedBeast}/>
         <Footer/>
-      </header>
     </div>
   );
+}
 }
 
 export default App;
--- src/hornedbeast.js
+++ src/hornedbeast.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Selected from "./selectedBeast";
 import Card from 'react-bootstrap/Card';
 
 
@@ -8,30 +7,22 @@ class HornedBeast extends React.Component {
         super(props);
         this.state ={
             count : 0 ,
-            isOpen: false
         };
     }
 
     favorite = () =>{
         this.setState({count : this.state.count +1})
+        this.props.displayModal (this.props.data)
     }
 
-    changeState = ()=>{
-        if(this.state.isOpen){
-            this.setState({isOpen : false})
-        }else{
-            this.setState({isOpen : true})
-        }
-    }
  
     render() {
         
 
         return (
             <>
-            <Selected changeState={this.changeState} checkIsOpen={this.state.isOpen}/>
                 <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={this.props.src} onClick={this.favorite}/>
+                    <Card.Img variant="top" src={this.props.src} alt={this.props.title} onClick={this.favorite}/>
                     <Card.Body>
                         <Card.Title>{this.props.title}  ❤️{this.state.count} </Card.Title>
                         <Card.Text>
