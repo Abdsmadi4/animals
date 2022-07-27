3aaac81c42b58921447a8b2ffae90525f99498c5
Abdallah Smadi <abd.smadi41@gmail.com>
2022-07-27 22:23:08 +0300

    add filter

--- src/main.js
+++ src/main.js
@@ -2,22 +2,34 @@ import React from "react";
 import HornedBeast from "./hornedbeast";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
+class Main extends React.Component {
 
-class Main extends React.Component{
-    
-    render(){
+    render() {
         return (
-            <Row xs={1} md={2} lg={3}>
+            <>
+                <Form.Select aria-label="Default select example" id="hornesSelector" onChange={this.props.handleSelect}>
+                    <option>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                    <option value="0">Else</option>
+                </Form.Select>
 
-                {
-                 this.props.allBeast.map(val => 
-                    <Col>
-                    <HornedBeast key={val.title} title={val.title} src={val.image_url} des={val.description} displayModal={this.props.displayModal}  data={val}/>
-                    </Col>
-                 )
-                }
-            </Row>
+                <Row xs={1} md={2} lg={3}>
+
+                    {
+                        this.props.allBeast.map(val =>
+                            <Col>
+                                <HornedBeast key={val.title} title={val.title} src={val.image_url} des={val.description} displayModal={this.props.displayModal} data={val} />
+                            </Col>
+                        )
+                    }
+                </Row>
+
+            </>
+
         );
     }
 }
